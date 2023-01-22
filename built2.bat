43311885fcb6512df0df2dbfa0363c3f24eca543
zhuzzzhha <90785698+zhuzzzhha@users.noreply.github.com>
2023-01-22 22:29:34 +0300

    Update built2.bat

--- built2.bat
+++ built2.bat
@@ -4,5 +4,5 @@ set "boostlib_dir=C:\boost_1_80_0\lib\x64\lib\"
 set "lib_path=%cd%\lib"
 set "include_path=%lib_path%\include"
 cmake -DBoost_INCLUDE_DIR=%boost_dir% -DBoost_LIBRARY_DIR_RELEASE=%boostlib_dir% -DCURL_INCLUDE_DIR=%include_path% -DOPENSSL_INCLUDE_DIR=%include_path% -G "Visual Studio 16 2019" -S . -B ./build 
-rem cmake --build ./build
-cmake --open ./build
+cmake --build ./build
+rem cmake --open ./build
