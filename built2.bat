9d4c545517ade148237487d53beff7522151ca5e
zhuzzzhha <90785698+zhuzzzhha@users.noreply.github.com>
2023-01-27 21:57:00 +0300

    Delete built2.bat

--- built2.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set "boost_dir=C:\boost_1_80_0\"
-set "boostlib_dir=C:\boost_1_80_0\lib\x64\lib\"
-set "lib_path=%cd%\lib"
-set "include_path=%lib_path%\include"
-cmake -DBoost_INCLUDE_DIR=%boost_dir% -DBoost_LIBRARY_DIR_RELEASE=%boostlib_dir% -DCURL_INCLUDE_DIR=%include_path% -DOPENSSL_INCLUDE_DIR=%include_path% -G "Visual Studio 16 2019" -S . -B ./build 
-rem cmake --build ./build
-rem cmake --open ./build
